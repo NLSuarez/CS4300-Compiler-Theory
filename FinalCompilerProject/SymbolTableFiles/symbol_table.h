904a443a69a7577f7acd5d085aa9d33e11ae10d3
Thomas Ferguson <erniecat7@comcast.net>
2017-04-03 21:18:14 -0700

    Update symbol_table.h
    Removed static qualifiers from the function prototypes.

--- FinalCompilerProject/SymbolTableFiles/symbol_table.h
+++ FinalCompilerProject/SymbolTableFiles/symbol_table.h
@@ -9,10 +9,10 @@ struct symbol_node
 };
 
 /* Generates hash key for given symbol */
-static unsigned int hash(char* symbol);
+unsigned int hash(const char* symbol);
 
 /* Looks for symbol in table, adds if not there, returns address of symbol in table */
-static char* lookup(struct symbol_node* symbol_table, char* symbol);
+char* lookup(struct symbol_node* symbol_table, const char* symbol);
 
 /* Generates a new, empty symbol table */
-static struct symbol_node* generateSymbolTable(unsigned int table_size = 9997);
+struct symbol_node* generateSymbolTable(unsigned int table_size = 9997);
