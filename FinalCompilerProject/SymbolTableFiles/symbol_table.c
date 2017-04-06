91262eb48689952b858390f9977e4eab95689b1b
Thomas Ferguson <erniecat7@comcast.net>
2017-04-05 19:40:16 -0700

    Update symbol_table.c
    Even more updates!
    Added char* array that contains C keywords listed in keywords.txt, located in this GitHub directory.
    Extern'd the DEBUG flag over into this file.
    Made a typedef for struct symbol_record*, called SYMBOL_TABLE.  Used where appropriate.
    Changed variables in some functions from symbol_table to symTab, to minimize confusion with new typedef.
    Made a function (populateSymbolTable) that inserts the C keywords into the symbol table, setting the appropriate kind values.
    Added DEBUG output in populateSymbolTable that is printed with DEBUG flag (-d) is included in command line.  It prints the kind and address of the structs containing the C keywords being added to the symbol table.

--- FinalCompilerProject/SymbolTableFiles/symbol_table.c
+++ FinalCompilerProject/SymbolTableFiles/symbol_table.c
@@ -3,76 +3,107 @@
 #include <stdio.h>
 #include <string.h>
 
+extern unsigned int DEBUG;
+
+// This should be kept up-to-date if more keywords are added to the set.
+const unsigned int KEYWORD_COUNT = 11;
+
+const char* C_KEYWORD_ARRAY[KEYWORD_COUNT] = { "cin", "cout", "else", "endl", "float", "if",
+                                               "int", "return", "while", "var", "const" };
+
+
 unsigned int hash(const char* symbol)
 {
-        unsigned int hashKey = 0;
-        int i;
-        for (i = 0; symbol[i] != 0; ++i)
-                hashKey += symbol[i];
+	unsigned int hashKey = 0;
+	int i;
+	for (i = 0; symbol[i] != 0; ++i)
+		hashKey += symbol[i];
 
-        hashKey = hashKey % 9997;
+	hashKey = hashKey % 9997;
 
-        return hashKey;
+	return hashKey;
 }
 
-struct symbol_record* lookup(struct symbol_record* symbol_table, const char* symbol)
+struct symbol_record* lookup(SYMBOL_TABLE symTab, const char* symbol)
 {
-        // Hash the key.
-        unsigned int hashKey = hash(symbol);
+	// Hash the key.
+	unsigned int hashKey = hash(symbol);
 
-        // Pointer to entry-point in symbol table.
-        struct symbol_record* listPtr = &symbol_table[hashKey];
+	// Pointer to entry-point in symbol table.
+	struct symbol_record* listPtr = &symTab[hashKey];
 
-        if (listPtr->symbol == NULL)
-        {
-                // New symbol AND current list is empty.
-                listPtr->symbol = (char*) malloc(strlen(symbol) + 1);
-                strcpy(listPtr->symbol, symbol);
-        }
-        else
-        {
-                // Current list is not empty; new symbol status unknown.
-                while (listPtr->next != NULL)
-                {
-                        if (strcmp(listPtr->symbol, symbol) == 0)
-                                return listPtr;         // Symbol found, return pointer to record.
+	if (listPtr->symbol == NULL)
+	{
+		// New symbol AND current list is empty.
+		listPtr->symbol = (char*) malloc(strlen(symbol) + 1);
+		strcpy(listPtr->symbol, symbol);
+	}
+	else
+	{
+		// Current list is not empty; new symbol status unknown.
+		while (listPtr->next != NULL)
+		{
+			if (strcmp(listPtr->symbol, symbol) == 0)
+				return listPtr;		// Symbol found, return pointer to record.
 
-                        listPtr = listPtr->next;
-                }
+			listPtr = listPtr->next;
+		}
 
-                // Check final node
-                if (strcmp(listPtr->symbol, symbol) == 0)
-                        return listPtr;                 // Symbol found, return pointer to record.
-                else
-                {
-                        // New symbol, append to end of current list
-                        listPtr->next = (struct symbol_record*) malloc(sizeof(struct symbol_record));
-                        listPtr = listPtr->next;
+		// Check final node
+		if (strcmp(listPtr->symbol, symbol) == 0)
+			return listPtr;			// Symbol found, return pointer to record.
+		else
+		{
+			// New symbol, append to end of current list
+			listPtr->next = (struct symbol_record*) malloc(sizeof(struct symbol_record));
+			listPtr = listPtr->next;
 
-                        listPtr->next = NULL;
-                        listPtr->symbol = (char*) malloc(strlen(symbol) + 1);
-                        strcpy(listPtr->symbol, symbol);
-                }
-        }
+			listPtr->next = NULL;
+			listPtr->symbol = (char*) malloc(strlen(symbol) + 1);
+			strcpy(listPtr->symbol, symbol);
+		}
+	}
 
-        return listPtr;
+	return listPtr;
 }
 
-struct symbol_record* generateSymbolTable(unsigned int tableSize)
+SYMBOL_TABLE generateSymbolTable(unsigned int tableSize)
 {
-        // Allocate memory for symbol table
-        struct symbol_record* symbol_table =
-                (struct symbol_record*) malloc(tableSize * sizeof(struct symbol_record));
+	// Allocate memory for symbol table
+	SYMBOL_TABLE symTab = (SYMBOL_TABLE) malloc(tableSize * sizeof(struct symbol_record));
 
-        if (symbol_table == NULL)
-                return NULL;    // Couldn't allocate memory
+	if (symTab == NULL)
+		return NULL;	// Couldn't allocate memory
 
-        // Initialize all elements of new table to NULL
-        for (unsigned int i = 0; i < tableSize; ++i)
-        {
-                symbol_table[i].symbol = NULL;
-                symbol_table[i].next = NULL;
-        }
+	// Initialize all elements of new table to NULL
+	for (unsigned int i = 0; i < tableSize; ++i)
+	{
+		symTab[i].symbol = NULL;
+		symTab[i].next = NULL;
+		symTab[i].kind = EMPTY;
+	}
 
-        return symbol_table;
+	populateSymbolTable(symTab);
+
+	return symTab;
+}
+
+void populateSymbolTable(SYMBOL_TABLE symTab)
+{
+	if (symTab == NULL)
+		return;
+
+	struct symbol_record* recordPtr = NULL;
+	for (int i = 0; i < KEYWORD_COUNT; ++i)
+	{
+		recordPtr = lookup(symTab, C_KEYWORD_ARRAY[i]);
+		recordPtr->kind = (enum symbol_type)(i + 1);
+		if (DEBUG)
+		printf("\nKEYWORD %s added:\t\tkind = (%d)\t\taddress = (0x%llX)\n",
+		       recordPtr->symbol, recordPtr->kind, (long long) recordPtr);;
+	}
+
+	if (DEBUG) printf("\n");
+
+	return;
 }
