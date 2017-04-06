ec86de91b6dce340fa238a1deecbe3e2c1ef1455
Thomas Ferguson <erniecat7@comcast.net>
2017-04-05 18:06:59 -0700

    Update driver.c
    lookup function now returns the address of the record (the struct in the symbol table's linked list), rather than the string.  Changes were made to driver.c to reflect this change.
    
    The loop now spaces output better.

--- FinalCompilerProject/SymbolTableFiles/driver.c
+++ FinalCompilerProject/SymbolTableFiles/driver.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 #include "symbol_table.h"
 
-const int TABLE_SIZE = 9997;
+const unsigned int TABLE_SIZE = 9997;
 
 int main(int argc, char** argv)
 {
-        struct symbol_node* symbol_table = generateSymbolTable(TABLE_SIZE);
+        struct symbol_record* symbol_table = generateSymbolTable(TABLE_SIZE);
+        struct symbol_record* recordPtr = NULL;
         char* str = (char*) malloc(256);
 
         while (1)
         {
                 scanf("%s", str);
                 int key = hash(str);
-                long long mem_adr = (long long) lookup(symbol_table, str);
+                recordPtr = lookup(symbol_table, str);
 
-                printf("Addresss of %s: %llX\n", str, (long long)mem_adr);
+                printf("Addresss of %s:\t\t0x%llX\n", str, (long long) recordPtr);
+
+                if (getc(stdin) == '\n') printf("\n");
         }
 
         return 0;
 }
+
