a9938ef5e97324d671e09fc3c21c4d6af8c3e076
Thomas Ferguson <erniecat7@comcast.net>
2017-04-05 18:30:27 -0700

    Update symbol_table.h
    Added enum to keep track of symbol "kind" as specified in part 5; note that the list is tentative.
    
    Added an extern const for the table size.
    
    Changed symbol_node to symbol_record, added field for symbol_type enum.
    
    Changed return types of lookup and generatedSymbolTable to struct symbol_record*.

--- FinalCompilerProject/SymbolTableFiles/symbol_table.h
+++ FinalCompilerProject/SymbolTableFiles/symbol_table.h
@@ -1,19 +1,22 @@
 #pragma once
 
+enum symbol_type { EMPTY = 0, CIN, COUT, ELSE, ENDL, FLOAT, IF, INT, RETURN, WHILE, VAR, CONSTANT };
+extern const unsigned int TABLE_SIZE;
+
 /* Building block of the linked-list-based symbol table */
-/* The symbol table will be an array of struct symbol_node pointers */
-struct symbol_node
+/* The symbol table will be an array of pointers to struct symbol_record  */
+struct symbol_record
 {
         char* symbol;
-        struct symbol_node* next;
+        enum symbol_type kind;
+        struct symbol_record* next;
 };
 
 /* Generates hash key for given symbol */
 unsigned int hash(const char* symbol);
 
-/* Looks for symbol in table, adds if not there, returns address of symbol in table */
-char* lookup(struct symbol_node* symbol_table, const char* symbol);
+/* Looks for symbol in table, adds if not there, returns address of record that contains symbol */
+struct symbol_record* lookup(struct symbol_record* symbol_table, const char* symbol);
 
 /* Generates a new, empty symbol table */
-struct symbol_node* generateSymbolTable(unsigned int table_size);
-
+struct symbol_record* generateSymbolTable(unsigned int table_size);
