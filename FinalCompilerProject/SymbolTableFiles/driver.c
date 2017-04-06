e3b1cbf0d44f1061e5ffc37e660267ce49789185
Thomas Ferguson <erniecat7@comcast.net>
2017-04-05 19:35:19 -0700

    Update driver.c
    Even more updates!
    Added detection of DEBUG flag (-d) in command line.

--- FinalCompilerProject/SymbolTableFiles/driver.c
+++ FinalCompilerProject/SymbolTableFiles/driver.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "symbol_table.h"
 
 const unsigned int TABLE_SIZE = 9997;
 
+unsigned int DEBUG = 0;
+
 int main(int argc, char** argv)
 {
+	if (argc > 1)
+	{
+		for(int i = 1; i < argc; ++i)
+			if (strcmp(argv[i], "-d") == 0) DEBUG = 1;
+	}
+
         struct symbol_record* symbol_table = generateSymbolTable(TABLE_SIZE);
         struct symbol_record* recordPtr = NULL;
-        char* str = (char*) malloc(256);
+	char* str = (char*) malloc(256);
 
         while (1)
         {
@@ -18,8 +27,8 @@ int main(int argc, char** argv)
 
                 printf("Addresss of %s:\t\t0x%llX\n", str, (long long) recordPtr);
 
-                if (getc(stdin) == '\n') printf("\n");
-        }
+		if (getc(stdin) == '\n') printf("\n");
+	}
 
         return 0;
 }
