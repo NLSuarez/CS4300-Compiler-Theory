096c496d031bb43d60b2892f425ec127afe3e3b6
TViss <TViss@users.noreply.github.com>
2017-04-25 00:37:48 -0700

    added comments
    made code more readable

--- FinalCompilerProject/BackupFolder/driver.c
+++ FinalCompilerProject/BackupFolder/driver.c
@@ -3,44 +3,47 @@
  
 int main(int argc, char **argv)
 {
-	if(argc > 1)
+	if(argc > 1)    //ensures function was passed source file name
 	{
 		char* filename = argv[1];
-		FILE *f = fopen(filename, "r"); //"r" to read
+		FILE *f = fopen(filename, "r");    //"r" to read
 		char *dot;
 		int  dotIndex;
-		dot = strrchr(filename, '.');
+		dot = strrchr(filename, '.');    //sets pointer to last dot in source name
 		char *slash;
 		int slashIndex;
-		slash = strrchr(filename, '/');
+		slash = strrchr(filename, '/');    //sets pointer to last slash in source name
+		
 		int hasDot = dot != NULL;
 		if (hasDot) dotIndex = (int) (dot - filename);
 		else dotIndex = -1;
 //	printf("dot found at %d\n", dotIndex);
+		
 		int hasSlash = slash != NULL;
 		if (hasSlash) slashIndex = (int) (slash - filename);
 		else slashIndex = -1;
 //	printf("slash found at %d\n", slashIndex);
+		
 		char outputFilename[256];
-		memset(outputFilename, '\0', sizeof(outputFilename));		
+		memset(outputFilename, '\0', sizeof(outputFilename));	
 		/* if ((hasDot) && (dotIndex > slashIndex))
 			I think this functions the same because 
 			slashIndex is -1 when !hasSlash */
 		if(((hasDot && hasSlash) && (dotIndex > slashIndex)) || (hasDot && !hasSlash)) 
 		{
-			//puts("remove extension");
-			strncpy(outputFilename, filename, dotIndex);
-			//puts(outoutFilename);
+	//	puts("remove extension");
+			strncpy(outputFilename, filename, dotIndex);    //copy filename up to dot into outputFilename
+	//	puts(outoutFilename);
 		}
 		else
 		{
-			strcpy(outputFilename, filename);
+			strcpy(outputFilename, filename);    //copy filename into outputFilename
 		}
 //	puts(outputFilename);
-		strcat(outputFilename, ".q");
+		strcat(outputFilename, ".q");    //appends .q to outputFilename
 		puts(outputFilename);
-		FILE *fp = fopen(outputFilename, "w+");
+		FILE *fp = fopen(outputFilename, "w+");    //opens new writeable file with outputFilename
 	}	
-	else puts("Error: no source file given");
+	else puts("Error: no source file given");    //print error if argc <= 1
 	return 0;
 }
