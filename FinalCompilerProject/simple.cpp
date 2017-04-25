8625b5410539465a98857d53702e134590d253a2
Thomas Ferguson <erniecat7@comcast.net>
2017-04-25 09:42:45 -0700

    Create simple.cpp

---
+++ FinalCompilerProject/simple.cpp
@@ -0,0 +1,15 @@
+// Simple test program - feel free to edit.
+#include <iostream>
+
+int a, b, c;
+float pi, tau, e;
+
+int main(int a1, int a2, float b1, float b2)
+{
+	int x;
+	int z;
+	x = 5;
+	z = 3.0;
+	cout << x << " " << z << " " << x + z << endl;
+	return 0;
+}
